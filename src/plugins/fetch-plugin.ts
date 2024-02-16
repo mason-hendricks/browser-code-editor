7b8e5eca9d4b4d8755d19ddb469394283943d69c
Mason Hendricks <77632298+mason-hendricks@users.noreply.github.com>
2024-02-16 14:05:11 -0600

    fetch-plugin: allow for loading of js and css files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,13 +11,56 @@ export const fetchPlugin = (input: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: input,
-          };
+
+      // load function to handle loading index.js of package
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: input,
+        };
+      })
+
+
+      // load function to handle css files
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+
+        // check to see if we have already fetched this file
+        // and see if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult
         }
+
+        const { data, request, } = await axios.get(args.path)
+
+        const escapedCssString = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escapedCssString}';
+          document.head.appendChild(style)
+        `
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname
+        }
+
+        // store response in cache
+        await fileCache.setItem(args.path, result)
+
+        return result;
+      })
+
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // load function for javascript files
 
         // check to see if we have already fetched this file
         // and see if it is in the cache
