a54f3b5dba02472df12e730df1200b83a4264468
Mason Hendricks <77632298+mason-hendricks@users.noreply.github.com>
2024-02-28 14:33:18 -0600

    refactor react side to switch from insert cell before to after

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -12,16 +12,16 @@ const CellList: React.FC = () => {
   const cellList = cells.map((cell) => {
     return (
       <Fragment key={cell.id}>
-        <AddCell nextCellId={cell.id} />
         <CellListItem cell={cell} />
+        <AddCell previousCellId={cell.id} />
       </Fragment>
     );
   });
 
   return (
     <div>
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {cellList}
-      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
     </div>
   );
 };
